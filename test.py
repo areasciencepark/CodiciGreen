1f44976f02094c5ae8bd29838b3bc587a19601fc
Fabio Morea <79448231+fabio-morea-areasciencepark@users.noreply.github.com>
2022-06-28 16:15:26 +0200

    test for drop NA

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
-
 import pandas as pd 
   
 raw_data = pd.read_excel("test_data_1.xlsx", dtype=str)
 raw_data = raw_data[[ "IPC"]]
-
 raw_data.dropna(inplace=True)
-print(len(raw_data)) #expected value: 3
+assert len(raw_data) == 3
+print("Test passed :-D")
 

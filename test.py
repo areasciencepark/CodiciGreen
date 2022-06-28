c8ce19be689e180625ee2ae3f85daee40a7b7ef0
Fabio Morea <79448231+fabio-morea-areasciencepark@users.noreply.github.com>
2022-06-28 16:39:11 +0200

    small updates

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
+# This is a basic test
+# to check that the algorithm actually drops empty lines
 import pandas as pd 
-  
+ 
 raw_data = pd.read_excel("test_data_1.xlsx", dtype=str)
 raw_data = raw_data[[ "IPC"]]
+assert len(raw_data) == 4
+
 raw_data.dropna(inplace=True)
+
 assert len(raw_data) == 3
 print("Test passed :-D")
-
